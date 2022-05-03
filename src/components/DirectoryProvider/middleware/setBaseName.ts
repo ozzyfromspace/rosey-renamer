eabd37e86677db533a40c5e259c387581ac7896d
ozzyfromspace <float.circuit@gmail.com>
2022-05-03 18:24:17 +0000

    pad the digit that is appended to the filename, to account for weird ordering schemas on third-party platforms

--- src/components/DirectoryProvider/middleware/setBaseName.ts
+++ src/components/DirectoryProvider/middleware/setBaseName.ts
@@ -1,7 +1,7 @@
 import {
   CardData,
   DirectoryState,
-  SetBaseNamePayload
+  SetBaseNamePayload,
 } from '../../utils/types';
 
 const setBaseName = (
@@ -11,14 +11,24 @@ const setBaseName = (
   let offset = 0;
   const cards = state.cards;
 
+  const nrCards = state.cards.length;
+
+  const log10 = (nr: number) => Math.log(Math.abs(nr) || 1) / Math.log(10);
+  const nrDigits = Math.ceil(log10(nrCards));
+
   const newCards: CardData[] = cards.map((card, index) => {
     if (!card.isLocked) {
-      card.imageData.currentName = `${payload.baseName}${index - offset + 1}`;
+      const initDigit = `${index - offset + 1}`;
+      const padding = [...Array(nrDigits - initDigit.length)].fill(0).join('');
+      const digit = padding + initDigit;
+
+      card.imageData.currentName = `${payload.baseName}${digit}`;
     } else {
       offset--;
     }
     return card;
   });
+
   return { ...state, cards: newCards };
 };
 
